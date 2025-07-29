9ac677d05bab26a6bbed749c2241cb585595bb5d
dbouget <bougetd@gmail.com>
2025-07-29 13:04:20 +0200

    Bump to newest gradio version

--- demo/src/gui.py
+++ demo/src/gui.py
@@ -1,7 +1,9 @@
 import os
 
 import gradio as gr
-from gradio_shinymodel3d import ShinyModel3D
+# from gradio_shinymodel3d import ShinyModel3D
+from zipfile import ZipFile
+from PIL import Image
 
 from .convert import nifti_to_obj
 from .css_style import css
@@ -23,36 +25,39 @@ class WebUI:
         cwd: str = "/home/user/app/",
         share: int = 1,
     ):
+        self.file_output = None
+        self.model_selector = None
+        self.stripped_cb = None
+        self.registered_cb = None
+        self.run_btn = None
+        self.slider = None
+        self.download_file = None
+
         # global states
         self.images = []
         self.pred_images = []
-
-        # @TODO: This should be dynamically set based on chosen volume size
-        self.nb_slider_items = 820
 
         self.model_name = model_name
         self.cwd = cwd
         self.share = share
 
-        self.class_name = "Lymph Nodes"  # default
+        self.class_name = "LymphNodes"  # default
         self.class_names = {
-            "Lymph Nodes": "CT_LymphNodes",
+            "LymphNodes": "CT_LymphNodes",
         }
 
         self.result_names = {
-            "Lymph Nodes": "LymphNodes",
+            "LymphNodes": "LymphNodes",
         }
 
-        # define widgets not to be rendered immediantly, but later on
-        self.slider = gr.Slider(
-            minimum=1,
-            maximum=self.nb_slider_items,
-            value=1,
-            step=1,
-            label="Which 2D slice to show",
+        self.volume_renderer = gr.Model3D(
+            clear_color=[0.0, 0.0, 0.0, 0.0],
+            label="3D Model",
+            visible=True,
+            elem_id="model-3d",
+            height=512,
         )
-        # self.volume_renderer = gr.Model3D(
-        self.volume_renderer = ShinyModel3D()
+        # self.volume_renderer = ShinyModel3D()
 
     def set_class_name(self, value):
         LOGGER.info(f"Changed task to: {value}")
@@ -83,25 +88,72 @@ class WebUI:
         LOGGER.info("Loading prediction volume to numpy..")
         self.pred_images = load_pred_volume_to_numpy("./prediction.nii.gz")
 
-        return "./prediction.obj"
+        slider = gr.Slider(
+            minimum=0,
+            maximum=len(self.images) - 1,
+            value=int(len(self.images) / 2),
+            step=1,
+            label="Which 2D slice to show",
+            interactive=True,
+        )
+
+        return "./prediction.obj", slider
 
     def get_img_pred_pair(self, k):
-        k = int(k)
-        out = gr.AnnotatedImage(
-            self.combine_ct_and_seg(self.images[k], self.pred_images[k]),
-            visible=True,
-            elem_id="model-2d",
-        )
-        return out
+        img = self.images[k]
+        img_pil = Image.fromarray(img)
+        seg_list = []
+        seg_list.append((self.pred_images[k], self.class_name))
+        return img_pil, seg_list
 
     def toggle_sidebar(self, state):
         state = not state
         return gr.update(visible=state), state
 
+    def package_results(self):
+        """Generates text files and zips them."""
+        output_dir = "temp_output"
+        os.makedirs(output_dir, exist_ok=True)
+
+        zip_filename = os.path.join(output_dir, "generated_files.zip")
+        with ZipFile(zip_filename, 'w') as zf:
+            zf.write("./prediction.nii.gz")
+
+        return zip_filename
+
+    def setup_interface_outputs(self):
+        with gr.Row():
+            with gr.Group():
+                with gr.Column(scale=2):
+                    t = gr.AnnotatedImage(
+                        visible=True,
+                        elem_id="model-2d",
+                        color_map={self.class_name: "#ffae00"},
+                        height=512,
+                        width=512,
+                    )
+
+                    self.slider = gr.Slider(
+                        minimum=0,
+                        maximum=1,
+                        value=0,
+                        step=1,
+                        label="Which 2D slice to show",
+                        interactive=True,
+                    )
+
+                    self.slider.change(fn=self.get_img_pred_pair, inputs=self.slider, outputs=t)
+
+            with gr.Group():
+                self.volume_renderer.render()
+                self.download_btn = gr.DownloadButton(label="Download results", visible=False)
+                self.download_file = gr.File(label="Download Zip", interactive=True, visible=False)
+
+
     def run(self):
         with gr.Blocks(css=css) as demo:
             with gr.Row():
-                with gr.Column(visible=True, scale=0.2) as sidebar_left:
+                with gr.Column(scale=1, visible=True) as sidebar_left:
                     logs = gr.Textbox(
                         placeholder="\n" * 16,
                         label="Logs",
@@ -115,11 +167,13 @@ class WebUI:
                         container=True,
                         # line_breaks=True,
                     )
-                    demo.load(read_logs, None, logs, every=1)
+                    timer = gr.Timer(value=1, active=True)
+                    timer.tick(fn=read_logs, inputs=None, outputs=logs)
+                    # demo.load(read_logs, None, logs, every=0.5)
 
-                with gr.Column():
+                with gr.Column(scale=2):
                     with gr.Row():
-                        with gr.Column(scale=0.2, min_width=150):
+                        with gr.Column(min_width=150):
                             sidebar_state = gr.State(True)
 
                             btn_toggle_sidebar = gr.Button(
@@ -135,36 +189,25 @@ class WebUI:
                             btn_clear_logs = gr.Button("Clear logs", elem_id="logs-button")
                             btn_clear_logs.click(flush_logs, [], [])
 
-                        file_output = gr.File(file_count="single", elem_id="upload")
-                        file_output.upload(self.upload_file, file_output, file_output)
+                        self.file_output = gr.File(file_count="single", elem_id="upload")
 
-                        model_selector = gr.Dropdown(
+                        self.model_selector = gr.Dropdown(
                             list(self.class_names.keys()),
                             label="Task",
                             info="Which structure to segment.",
                             multiselect=False,
                         )
-                        model_selector.input(
-                            fn=lambda x: self.set_class_name(x),
-                            inputs=model_selector,
-                            outputs=None,
-                        )
 
-                        with gr.Column(scale=0.2, min_width=150):
-                            run_btn = gr.Button("Run analysis", variant="primary", elem_id="run-button")
-                            run_btn.click(
-                                fn=lambda x: self.process(x),
-                                inputs=file_output,
-                                outputs=self.volume_renderer,
-                            )
+                        with gr.Column(min_width=150):
+                            self.run_btn = gr.Button("Run segmentation", variant="primary", elem_id="run-button")
 
                     with gr.Row():
                         gr.Examples(
                             examples=[
                                 os.path.join(self.cwd, "test_thorax_CT.nii.gz"),
                             ],
-                            inputs=file_output,
-                            outputs=file_output,
+                            inputs=self.file_output,
+                            outputs=self.file_output,
                             fn=self.upload_file,
                             cache_examples=False,
                         )
@@ -176,21 +219,17 @@ class WebUI:
                             """
                         )
 
-                    with gr.Row():
-                        with gr.Column():
-                            # create dummy image to be replaced by loaded images
-                            t = gr.AnnotatedImage(visible=True, elem_id="model-2d")
+                    self.setup_interface_outputs()
 
-                            self.slider.input(
-                                self.get_img_pred_pair,
-                                self.slider,
-                                t,
-                            )
-
-                            self.slider.render()
-
-                        self.volume_renderer.render()
-
+            # Define the signals/slots
+            self.file_output.upload(self.upload_file, self.file_output, self.file_output)
+            self.model_selector.input(fn=lambda x: self.set_class_name(x), inputs=self.model_selector, outputs=None)
+            self.run_btn.click(fn=self.process, inputs=[self.file_output],
+                               outputs=[self.volume_renderer, self.slider]).then(fn=lambda:
+            gr.DownloadButton(visible=True), inputs=None, outputs=self.download_btn)
+            self.download_btn.click(fn=self.package_results, inputs=[], outputs=self.download_file).then(fn=lambda
+                file_path: gr.File(label="Download Zip", visible=True, value=file_path), inputs=self.download_file,
+                                    outputs=self.download_file)
         # sharing app publicly -> share=True:
         # https://gradio.app/sharing-your-app/
         # inference times > 60 seconds -> need queue():
